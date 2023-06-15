d2f6922211673dae109cf82394d5eab1c762470f
Maheri29 <hei.mahery.2@gmail.com>
2023-06-15 11:52:13 +0300

    feat: update useLocalStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,19 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
 
-}
+const useLocalStorage = (key: string, initialValue: any) => {
+  // État local pour stocker la valeur
+  const [value, setValue] = useState(() => {
+    // Récupérer la valeur depuis le Local Storage ou utiliser la valeur initiale
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
 
-export {
-  useLocalStorage
-}
+  // Mettre à jour la valeur dans le Local Storage lorsqu'elle change
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorage };
