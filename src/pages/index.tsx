b7482af000f1f125a57691f9ecd09d75250297c8
Maheri29 <hei.mahery.2@gmail.com>
2023-06-15 10:31:00 +0300

    feat: add diff time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,61 @@
+import { useEffect, useState, useRef } from 'react';
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-/**
-  Calculates the time difference between the server time and client time.
-  @param {Date} serverTime - The server time.
-  @param {Date} clientTime - The client time.
-  @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
-*/
-const calculateTimeDifference = (server: Date, client: Date) => {};
+const calculateTimeDifference = (server, client) => {
+  const timeDifference = Math.abs(client.getTime() - server.getTime());
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+};
 
 export default function Home() {
   const router = useRouter();
+  const [clientTime, setClientTime] = useState(null);
+  const [serverTime, setServerTime] = useState(null);
+  const [timeDifference, setTimeDifference] = useState(null);
+
+  const serverTimeRef = useRef(null);
+  const clientTimeRef = useRef(null);
+
+  useEffect(() => {
+    const getClientTime = () => {
+      const newClientTime = new Date();
+      setClientTime(newClientTime);
+    };
+
+    getClientTime();
+
+    const interval = setInterval(getClientTime, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    const serverDateTime = new Date();
+    setServerTime(serverDateTime);
+  }, []);
+
+  useEffect(() => {
+    if (clientTime && serverTime) {
+      const difference = calculateTimeDifference(serverTime, clientTime);
+      setTimeDifference(difference);
+    }
+  }, [clientTime]);
+
   const moveToTaskManager = () => {
     router.push("/tasks");
-  }
+  };
+
   return (
     <>
       <Head>
-        <title>Web 2 - Exam TD</title>
+      <title>Web 2 - Exam TD</title>
         <meta name="description" content="Just an exam" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -26,19 +63,23 @@ export default function Home() {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime" ref={serverTimeRef}>
+              {serverTime ? serverTime.toLocaleString() : ""}
+            </span>
           </p>
-
-          {/* Display here the time difference between the server side and the client side */}
+          <p>
+            Client time:{" "}
+            <span className="clientTime" ref={clientTimeRef}>
+              {clientTime ? clientTime.toLocaleString() : ""}
+            </span>
+          </p>
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="timeDifference">{timeDifference}</span>
           </p>
         </div>
-
         <div>
           <button onClick={moveToTaskManager}>Go to task manager</button>
         </div>
